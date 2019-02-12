600b4f2f479ebaf6e570126f5fbfa195d41dc228
rocklee <rocklee.no21@gmail.com>
2019-02-12 17:40:56 +0530

    changing the color of the button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Person from './Person.js';
 import './index.css';
 
 class App extends Component {
   state = {
     persons: [
-      {name: 'Rigden', gender: 'Male'},
-      {name: 'Dichen', gender: 'Female'},
-      {name: 'Shubham', gender: 'Male'},
-      {name: 'Satyam', gender: 'Male'}
+      {id:'1', name: 'Rigden', gender: 'Male'},
+      {id:'2', name: 'Dichen', gender: 'Female'},
+      {id:'3', name: 'Shubham', gender: 'Male'},
+      {id:'4', name: 'Satyam', gender: 'Male'}
     ],
     showPerson: false
   };
@@ -18,21 +18,41 @@ class App extends Component {
   }
 
   deletePersonHandler = (personIndex) => {    //this function deletes an paragraph
-    const persons = this.state.persons;
+    const persons = [...this.state.persons];    //or u can also do  const persons =  this.state.persons.slice();
     persons.splice(personIndex, 1);
     this.setState({persons: persons})
   }
 
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    })
+  
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+
+    // or you can also do const person = Object.assign({}, this.state.persons[personIndex]);
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState( {persons: persons});
+
+  }
     render() {
       const style = {
-        backgroundColor: 'white',
+        backgroundColor: 'green',
+        color: 'white',
         font: 'inherit',
-        border: '1px solid red',
+        border: '1px solid blue',
         padding: '8px',
         cursor: 'pointer'
-      }
+      };
 
-      let persons = null;
+let persons = null;
 
   if ( this.state.showPerson ) {
 persons = (
@@ -41,15 +61,28 @@ persons = (
 				return <Person 
           click={() => this.deletePersonHandler(index)}  /* alternative would be this.deletePerson.bind(this, 'index') */
           name={person.name} 
-          gender={person.gender} />
+          gender={person.gender}
+          key={person.id} 
+          changed={(event) => this.nameChangedHandler(event, person.id)} />    /*key property is to be given a unique value*/
 			})}
         </div>
          );
+         style.backgroundColor='red';
   }
+
+  let classes = [];
+
+  if (this.state.persons.length <=2) {
+    classes.push('red');
+  }
+  if (this.state.persons.length <=1) {
+    classes.push('bold');
+  }
+
       return (
          <div className='App'>
           <h1>I'm an React App</h1>
-          <p>Is this really working?</p>
+          <p  className={classes.join(' ')}>Is this really working?</p>
           <button style={style} onClick={this.togglePersonHandler}>Toggle Me</button>  
            {persons}         
          </div>
