a2df3d9a693ff8fb220b210f8efaee4833743868
rocklee <rocklee.no21@gmail.com>
2019-02-12 10:39:06 +0530

    deleting and hiding components: 3rd commit

--- src/App.js
+++ src/App.js
@@ -1,105 +1,59 @@
-import React, { Component } from 'react';
-import Prop from './Prop';
+import React, {Component} from 'react';
+import Person from './Person.js';
 import './index.css';
 
-
-
 class App extends Component {
-state = {
-  person: [
-    {name: 'Rigden', gender: 'male'},
-    {name: 'Sonam', gender: 'female'},
-    {name: 'Tashi', gender: 'female'}
-  ],
-  otherState: 'some other state',
-  dataView: true
-}
-
-switchNameHandler = (newName) => {
-  this.setState( {      //setstate takes object as an  argument
-  person: [
-    {name: newName, gender: 'male'},
-    {name: 'Dichen', gender: 'female'},
-    {name: 'Pema', gender: 'female'}
-  ] 
-  }
-  );
-}
-
-onChangeHandler = (event) => {
-  this.setState({
-    person: [ 
-      {name: event.target.value, gender: 'male'},
-      {name: event.target.value, gender: 'female'},
-      {name: event.target.value, gender: 'female'}
+  state = {
+    persons: [
+      {name: 'Rigden', gender: 'Male'},
+      {name: 'Dichen', gender: 'Female'},
+      {name: 'Shubham', gender: 'Male'},
+      {name: 'Satyam', gender: 'Male'}
     ],
-    otherState: 'some other state'
-  })
-}
-
-toggleDataHandler = () => {     
-    const dataView = this.state.dataView;
-    this.setState({dataView: !dataView})
-}
-  render(){
-      return (
-        <div className="App">
-         <button onClick = {this.toggleDataHandler}>Hide</button>
-          { this.state.dataView === true ?
-          
-          <div>
-            <p>Is this really working</p>
-            <p>This is really working!</p>
-            <Prop 
-              name={this.state.person[0].name}
-              gender={this.state.person[0].gender}
-              click={this.switchNameHandler.bind(this, 'Tashi')}
-              changed={this.onChangeHandler} >or is he?
-            </Prop> 
-            <Prop 
-              name={this.state.person[1].name}
-              gender={this.state.person[1].gender}
-              changed={this.onChangeHandler}>Or is he?
-            </Prop>
-            <Prop 
-              name={this.state.person[2].name}
-              gender={this.state.person[2].gender}
-              changed={this.onChangeHandler}/>
-            <Prop
-              name={this.state.otherState} />
-         </div>: null
-          }
-        </div>
-      )
+    showPerson: false
+  };
+  togglePersonHandler = () => {
+    const showPerson = this.state.showPerson;												
+    this.setState({showPerson: !showPerson});
   }
+
+  deletePersonHandler = (personIndex) => {    //this function deletes an paragraph
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons})
+  }
+
+    render() {
+      const style = {
+        backgroundColor: 'white',
+        font: 'inherit',
+        border: '1px solid red',
+        padding: '8px',
+        cursor: 'pointer'
+      }
+
+      let persons = null;
+
+  if ( this.state.showPerson ) {
+persons = (
+		<div>
+			{this.state.persons.map((person, index) => {
+				return <Person 
+          click={() => this.deletePersonHandler(index)}  /* alternative would be this.deletePerson.bind(this, 'index') */
+          name={person.name} 
+          gender={person.gender} />
+			})}
+        </div>
+         );
+  }
+      return (
+         <div className='App'>
+          <h1>I'm an React App</h1>
+          <p>Is this really working?</p>
+          <button style={style} onClick={this.togglePersonHandler}>Toggle Me</button>  
+           {persons}         
+         </div>
+       );
+    };
 }
 export default App;
-
-// state = {
-//   person: [
-//     {name: 'Dichen' gender: 'Boy'},
-//     {name: 'Sachin' gender: 'Boy'}
-//   ]
-// }
-
-
-
-// const [ currentState, updatedState ] = useState({
-//   person: [
-//    {name: 'Dichen', gender: 'Boy'},
-//    {name: 'Sachin', gender: 'Boy'},
-//    {name: 'Shubham', gender: 'girl'}
-//    ],
-//    otherState: 'some data'
-// });
-
-// console.log(currentState);
-
-//  const switchNameHandler = () => {
-//      updatedState({
-//      person: [
-//        {name: 'Dichen', gender: 'Girl'},
-//        {name: 'Tandulkar', gender: 'Boy'},
-//        {name: 'Shubham', gender: 'boy'}
-//      ]
-//    })
